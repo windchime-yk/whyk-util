20102fb6d38c9834f4e95891cf438d500c775d44
WhyK <windchimeyk@gmail.com>
2020-10-24 17:05:00 +0900

    feat: optimized page

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -10,3 +10,21 @@ document.addEventListener('click', (e: HTMLElementEvent<HTMLAnchorElement>) => {
   const targetId = target.hash
   smoothScroll(targetId)
 })
+
+const replace = (): void => {
+  const base = {
+    ruby: '<ruby class="ns_ruby">$1<rt>$2</rt></ruby>',
+    emphasis: '<strong class="ns_emphasis">$1</strong>',
+  }
+  const reg = {
+    ruby: /[|｜](.+?)《(.+?)》/g,
+    emphasis: /《《(.+?)》》/g,
+  }
+  const obj = document.querySelector<HTMLTextAreaElement>('#Base').value
+  const replaceBody = obj
+    .replace(reg.ruby, base.ruby)
+    .replace(reg.emphasis, base.emphasis)
+  document.querySelector<HTMLTextAreaElement>('#Output').value = replaceBody
+}
+const replaceBtn = document.querySelector<HTMLButtonElement>('#ReplaceBtn')
+replaceBtn.addEventListener('click', replace)
